4ded7406384d874acc60e06c5f5d1581516add60
Mikel <mikel2dm3@gmail.com>
2020-12-10 11:18:22 +0100

    añadir xml

--- app/src/main/java/org/example/retocomerciales/MainActivity.java
+++ app/src/main/java/org/example/retocomerciales/MainActivity.java
@@ -2,10 +2,7 @@ package org.example.retocomerciales;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.res.AssetManager;
 import android.os.Bundle;
-
-import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,17 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*
-
-
-        try{
-            AssetManager gestor = getAssets();
-            InputStream entrada = gestor.open("partners.xml");
-        }catch (){
-
-        }
-
-         */
-
     }
 }
