30c54a07a5b93103862055399a8f1f08e3e8a720
Castas115 <75309501+castas115@users.noreply.github.com>
2020-12-10 10:09:26 +0100

    syso prueba

--- app/src/main/java/org/example/retocomerciales/MainActivity.java
+++ app/src/main/java/org/example/retocomerciales/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("hola mundo");
+        System.out.println("hola mund");
     }
 }
