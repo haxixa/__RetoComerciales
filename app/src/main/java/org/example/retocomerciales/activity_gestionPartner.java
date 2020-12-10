25b8d2d8b93c1679a9f5ed9ca7cd95ebb035b8c9
haxixa <56109212+haxixa@users.noreply.github.com>
2020-12-10 10:38:54 +0100

    Preparado(Layout y activities vacios)

--- app/src/main/java/org/example/retocomerciales/activity_gestionPartner.java
+++ app/src/main/java/org/example/retocomerciales/activity_gestionPartner.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class activity_gestionPartner extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_gestion_partner);
     }
 }
