d3f016db02ed7fee0a47dd1fb20a0f56eb7a573d
Castas115 <75309501+Castas115@users.noreply.github.com>
2020-12-10 11:22:14 +0100

    main con boton

--- app/src/main/java/org/example/retocomerciales/MainActivity.java
+++ app/src/main/java/org/example/retocomerciales/MainActivity.java
@@ -2,13 +2,35 @@ package org.example.retocomerciales;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button _iniciar;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _iniciar=findViewById(R.id.btn_iniciar);
+
+        _iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
+
+    /*public void lanzarPropuesta (){
+        Intent intent = new Intent(MainActivity.this, activity_menu.class);
+        intent.putExtra("nombre", nombre.getText().toString());
+        startActivityForResult(intent, 123);//si no vas devolver dato:  startActivity(intent);
+    }*/
 }
